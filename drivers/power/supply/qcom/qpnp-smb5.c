835f66c2c1bd6273f3924eda1b5b8d112092e385
momojuro <momojurosan@gmail.com>
2022-01-20 06:59:04 +0100

    power: smb5-lib: Demote spammy error log to debug
    [2.634775] batt power supply prop 4 not supported

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -1993,7 +1993,7 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 		val->intval = chg->fcc_stepper_enable;
 		break;
 	default:
-		pr_err("batt power supply prop %d not supported\n", psp);
+		pr_debug("batt power supply prop %d not supported\n", psp);
 		return -EINVAL;
 	}
 
